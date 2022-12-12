14f6f4543721e42bb69ad33d1ecdc39a62fc739a
irsyadmufid <irsyadm90@gmail.com>
2022-12-12 20:08:43 +0700

    fix

--- lib/Profile_User/detail.dart
+++ lib/Profile_User/detail.dart
@@ -34,7 +34,7 @@ class _DetailPageState extends State<DetailPage> {
                   margin:
                       const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
+                  decoration: BoxDecoration(
                     boxShadow: [
                       BoxShadow(
                         color: Colors.grey.withOpacity(0.5),
@@ -46,89 +46,195 @@ class _DetailPageState extends State<DetailPage> {
 
                     ],
                     borderRadius: BorderRadius.circular(20),
-                    color: const Color.fromARGB(255, 165, 224, 167),
+                    color: showColor(),
                   ), 
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "${model.nama}, ${model.umur}",
-                  style: const TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    
+
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+
+                      Text(
+                        "${model.nama}, ${model.umur}",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+
+                      const SizedBox(
+                        height: 5,
+                      ),
+
+                      Text(
+                        "\n\n\nJenis Kelamin : ${model.gender}\n",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+
+                      Text(
+                        "Status: ${model.vaksin == "Sudah" ? "Sudah ✅\n" : "Belum ❌\n"}",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+
+                      Text(
+                        "Asal Negara : ${model.negara}\n",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+
+                      Text(
+                        "Tujuan Kedatangan : ${model.tujuan}\n",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+
+                      Text(
+                        "Provinsi Tujuan : ${model.prov}\n",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+
+                      Text(
+                        "Kontak : ${model.kontak}\n",
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+
+
+                      const SizedBox(
+                        height: 20,
+                      ),
+
+                      const SizedBox(
+                      height: 20,
+                      ),
+
+                    ],
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "\n\n\n Jenis Kelamin : ${model.gender}",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                    "Status: ${model.vaksin == "Sudah" ? "Sudah ✅" : "Belum ❌"}"),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Asal Negara : ${model.negara}",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Tujuan Kedatangan : ${model.tujuan}",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Provinsi Tujuan : ${model.prov}",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Kontak : ${model.kontak}",
-                ),
-              ),
-              ]),
-              ),  
-              ),
-              Expanded(
-                  flex: 2,
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
+                Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
                           context,
                           MaterialPageRoute(
                               builder: (context) => const ProfilePage()),
                         );
-                      },
-                      style: TextButton.styleFrom(
-                          backgroundColor:
-                              Colors.blue,
-                          minimumSize: const Size.fromHeight(40.0),
-                      ),
-                      child: const Text(
-                        "Back",
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  )),
+                },
+                child: Text('Back', style: TextStyle(color: Colors.white)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple,
+                  minimumSize: const Size.fromHeight(60),
+                ),
+              )
             ],
           ),
-        ),
+        ]),
+      ),
+      // body: Container(
+      //   child: Padding(
+      //     padding: const EdgeInsets.all(30.0),
+      //     child: Column(
+      //         crossAxisAlignment: CrossAxisAlignment.stretch,
+      //         mainAxisSize: MainAxisSize.min,
+      //         children: [
+      //           Padding(
+      //             padding: const EdgeInsets.all(8.0),
+      //             child: Text(
+      //               "${model.nama}, ${model.umur}",
+      //               style: const TextStyle(
+      //                 fontSize: 20.0,
+      //                 fontWeight: FontWeight.bold,
+      //               ),
+      //             ),
+      //           ),
+      //           Padding(
+      //             padding: const EdgeInsets.all(8.0),
+      //             child: Text(
+      //               "\n\n\n Jenis Kelamin : ${model.gender}",
+      //             ),
+      //           ),
+      //           Padding(
+      //             padding: const EdgeInsets.all(8.0),
+      //             child: Text(
+      //                 "Status: ${model.vaksin == "Sudah" ? "Sudah ✅" : "Belum ❌"}"),
+      //           ),
+      //           Padding(
+      //             padding: const EdgeInsets.all(8.0),
+      //             child: Text(
+      //               "Asal Negara : ${model.negara}",
+      //             ),
+      //           ),
+      //           Padding(
+      //             padding: const EdgeInsets.all(8.0),
+      //             child: Text(
+      //               "Tujuan Kedatangan : ${model.tujuan}",
+      //             ),
+      //           ),
+      //           Padding(
+      //             padding: const EdgeInsets.all(8.0),
+      //             child: Text(
+      //               "Provinsi Tujuan : ${model.prov}",
+      //             ),
+      //           ),
+      //           Padding(
+      //             padding: const EdgeInsets.all(8.0),
+      //             child: Text(
+      //               "Kontak : ${model.kontak}",
+      //             ),
+      //           ),
+      //         Expanded(
+      //             flex: 2,
+      //             child: Align(
+      //               alignment: Alignment.bottomCenter,
+      //               child: TextButton(
+      //                 onPressed: () {
+      //                   Navigator.pushReplacement(
+      //                     context,
+      //                     MaterialPageRoute(
+      //                         builder: (context) => const ProfilePage()),
+      //                   );
+      //                 },
+      //                 style: TextButton.styleFrom(
+      //                     backgroundColor:
+      //                         Colors.blue,
+      //                     minimumSize: const Size.fromHeight(40.0),
+      //                 ),
+      //                 child: const Text(
+      //                   "Back",
+      //                   style: TextStyle(color: Colors.white),
+      //                 ),
+      //               ),
+      //             )),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
+  }
+
+  showColor() {
+    if (model.vaksin == "Sudah") {
+      return const Color.fromARGB(255, 165, 224, 167);
+    } else {
+      return Color.fromARGB(255, 231, 85, 85);
+    }
   }
 }
